40acf0491a10149ae4cea9f7a28b558815b421e0
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-16 21:00:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name="Squawk!", language="Ruby")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+def parrot(name="Squawk")
+  puts "#{name}!"
 end
